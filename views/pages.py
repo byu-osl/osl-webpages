eadb15b1f2c0f341e59868019afd4b1171d4461b
Daniel Zappala <daniel.zappala@gmail.com>
2013-07-06 12:16:52 -0600

    Deleted unused views.

--- views/pages.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-pages = Blueprint('pages', __name__)
-
-@pages.route('/', defaults={'page': 'index'})
-@pages.route('/<page>/')
-def show(page):
-    print "PAGE:",page
-    return render_template(page+'.html',active=page)
