0a653f39e220c46c79602295415e54d8332b4546
Daniel Zappala <daniel.zappala@gmail.com>
2013-07-06 12:04:38 -0600

    Setup for Flask frozen pages

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
+import sys
+
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
-from views.pages import pages
-app.register_blueprint(pages)
+# @app.route('/', defaults={'page': 'index'})
+@app.route('/')
+def index():
+    return render_template('index.html',active='index')
+
+@app.route('/<page>/')
+def show(page):
+    print "PAGE:",page
+    return render_template(page+'.html',active=page)
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
 
-if __name__ == '__main__': 
-    app.debug = True
-    app.run(host='0.0.0.0',port=8080)
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.debug = True
+        app.run(port=8080)
